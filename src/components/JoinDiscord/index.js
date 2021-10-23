088a028104adfc061051dd53e164bf45784b4cc3
Cory <coryitpro@gmail.com>
2021-10-23 17:00:30 -0400

    Add discord component

---
+++ src/components/JoinDiscord/index.js
@@ -0,0 +1,9 @@
+import "./style.scss"
+
+const JoinDiscord = () => (
+  <div className="discord container flex">
+    <div className="discord-wrapper"></div>
+  </div>
+)
+
+export default JoinDiscord
