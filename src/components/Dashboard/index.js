e525261c30f610016fa0762a80648c3540f07ba6
Cory <coryitpro@gmail.com>
2021-10-23 16:59:12 -0400

    Add dashboard container

---
+++ src/components/Dashboard/index.js
@@ -0,0 +1,15 @@
+import Navbar from "components/Navbar"
+import Hero from "components/Hero"
+import JoinDiscord from "components/JoinDiscord"
+
+import "./style.scss"
+
+const Dashboard = () => (
+  <>
+    <Navbar />
+    <Hero />
+    <JoinDiscord />
+  </>
+)
+
+export default Dashboard
