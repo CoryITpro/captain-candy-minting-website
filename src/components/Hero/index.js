5a2bba15a5affe8bef7e9685b270e6ee42d98f27
Cory <coryitpro@gmail.com>
2021-10-23 16:59:26 -0400

    Add hero component

---
+++ src/components/Hero/index.js
@@ -0,0 +1,39 @@
+import { CaptainCandyFaceRadial } from "resources/Images"
+import { Link } from "react-scroll"
+import "./style.scss"
+
+const Hero = () => (
+  <div className="hero container flex">
+    <div className="hero-caption">
+      <div className="hero-caption-title flex">
+        <img src={CaptainCandyFaceRadial} alt="hero title" />
+        <span>CAPTAIN CANDY</span>
+      </div>
+      <p>
+        Captain Candy is a part of the SuperKrass team and has now his own
+        collection of 10,000 unique NFTs available on the Ethereum blockchain.
+      </p>
+      <p>
+        Each Captain is completely unique, created using over 100 individual
+        features.
+      </p>
+      <div className="hero-caption-buttons">
+        <button>READ MORE</button>
+        <button>
+          <Link
+            activeClass="active"
+            to={"mint"}
+            spy={true}
+            smooth={true}
+            offset={0}
+            duration={500}
+          >
+            MINT NOw
+          </Link>
+        </button>
+      </div>
+    </div>
+  </div>
+)
+
+export default Hero
